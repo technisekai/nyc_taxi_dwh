dcdd92d5327cb2e3e1298b6886cc434da06c1591
Widi Afandi <widi.afandi@batumbu.id>
2025-05-04 20:19:31 +0700

    Revert "update: get schema and data types"
    This reverts commit 1058181c936b9da2e584ff9432da40f68740205e.

--- dags/dag-nyc-taxi-bronze.py
+++ dags/dag-nyc-taxi-bronze.py
@@ -33,10 +33,7 @@ def load_data(path: str, database: str, creds: dict):
     table_name = f"`{database}`.`{path.split('/')[-1].split('_')[0]}`"
     conn = connect(db_type="clickhouse", creds=creds)
     # Check is destination table exists
-    schema = dict(zip(
-        df.collect_schema().names(), 
-        df.collect_schema().dtypes()
-    ))
+    schema = dict(zip(df.columns, df.dtypes))
     clickhouse_create_table(conn, table_name, schema)
     # Inject data
     clickhouse_batch_load(conn, table_name, df)
